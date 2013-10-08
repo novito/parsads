1b8615ba80b4d33ed16e1315d8ff5a349baab510
Joan Roig <roigarderiu@gmail.com>
2013-10-08 19:45:01 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Parsads::Application.config.secret_key_base = 'a035f7a7dc398005a9b948ffc44894c1f71b5ca7575b830be8f417d5a91801b496355063e72e4a423f248410721b6bf5a33d8b8fafba9b9be9e0c9a6e9f831b0'
